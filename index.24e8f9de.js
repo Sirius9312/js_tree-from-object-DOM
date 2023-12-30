dd2b24457b66883b734f255fc8fad2587ba2e073
Sergey Arnautov <sirius9312@ukr.net>
2023-12-30 15:57:55 +0200

    Updates

---
+++ index.24e8f9de.js
@@ -0,0 +1,2 @@
+!function e(t,n){let r=document.createElement("ul");for(let t in n){let l=document.createElement("li");l.textContent=t,r.appendChild(l),"object"==typeof n[t]&&e(l,n[t])}t.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.24e8f9de.js.map
