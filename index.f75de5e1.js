785d40dcb023136d7726458cd25c38fa86068dd5
Sergey Arnautov <sirius9312@ukr.net>
2023-12-30 15:53:15 +0200

    Updates

---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    "Drink": {
+        "Wine": {},
+        "Schnaps": {}
+    },
+    "Fruit": {
+        "Red": {
+            "Cherry": {},
+            "Strawberry": {}
+        },
+        "Yellow": {
+            "Banana": {},
+            "Pineapple": {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const ulList = document.createElement("ul");
+    for(const key in data){
+        const liItem = document.createElement("li");
+        liItem.textContent = key;
+        ulList.appendChild(liItem);
+        if (typeof data[key] === "object") createTree(liItem, data[key]);
+    }
+    element.append(ulList);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
